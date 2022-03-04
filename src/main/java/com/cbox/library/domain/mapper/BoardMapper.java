d6e29dd750e635d8ff04ff3c4807866521bec4ce
kuzu <kt.0404.uran@gmail.com>
2022-03-04 11:08:58 +0900

    test

--- src/main/java/com/cbox/library/domain/mapper/BoardMapper.java
+++ src/main/java/com/cbox/library/domain/mapper/BoardMapper.java
@@ -1,6 +1,9 @@
 package com.cbox.library.domain.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.cbox.library.domain.model.Board;
@@ -10,5 +13,7 @@ public interface BoardMapper {
     
     @Select("SELECT * FROM board WHERE board_id = #{boardId}")
     public Board findByBoardId(int boardId);
+    
+    public List<Board> find(@Param("boardId") Integer boardId);
     
 }
