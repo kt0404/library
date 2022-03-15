fd352c90cd33d5dc7347c6e5dbb3bfc5f9b10c87
kuzu <kt.0404.uran@gmail.com>
2022-03-16 07:42:34 +0900

    県に変更

--- src/main/java/com/cbox/library/domain/auth/WebSecurityConfig.java
+++ src/main/java/com/cbox/library/domain/auth/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override 
+    public void configure(WebSecurity web) {
+        web.ignoring()
+            .antMatchers("/css/**")
+            .antMatchers("/images/**")
+            .antMatchers("/js/**");
+    }
     
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -24,10 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		        "/detail/**",
 		        "/delete/**",
 		        "/update/**",
-		        "/request/list",
-		        "/css/**",
-		        "/images/**",
-		        "/js/**"
+		        "/request/list"
 		        ).permitAll().anyRequest().authenticated();
 		http.formLogin().loginPage("/login").permitAll();
 		http.logout().logoutSuccessUrl("/").permitAll();
